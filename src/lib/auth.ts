aa4354df3ba1ffe8c5227bee73464568499fa3ed
David Pérez <deividpa022@gmail.com>
2025-02-01 13:59:47 -0500

    feat: enhance authentication flow with user validation and custom JWT encoding

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import GitHub from "next-auth/providers/github"
+import { v4 as uuid } from "uuid"
 import { prisma } from "./prisma"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { userSchema } from "./db/schemas/userSchema"
+import { encode } from "next-auth/jwt"
 
 const adapter = PrismaAdapter(prisma)
  
@@ -14,14 +17,51 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       password: { type: "password" },
     },
     authorize: async (credentials) => {
+
+      const validatedCredentials = await userSchema.parse(credentials)
+
       const user = await prisma.user.findFirst({
-        where: { email: credentials.email, password: credentials.password },
+        where: { email: validatedCredentials.email, password: validatedCredentials.password },
       })
 
       if(!user) {
-        return null
+        throw new Error("Invalid credentials");
       }
+
       return user;
     }
   })],
+  callbacks: {
+    async jwt({token, account}) {
+      if(account?.provider === "credentials") {
+        token.credentials = true;
+      }
+      return token;
+    }
+  },
+  jwt: {
+    encode: async function(params) {
+      if(params.token?.credentials) {
+        const sessionToken = uuid();
+
+        if(!params.token.sub) {
+          throw new Error("No userId found in token");
+        }
+
+        const createdSession = await adapter?.createSession?.({
+          sessionToken,
+          userId: params.token.sub,
+          expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+        })
+
+        if(!createdSession) {
+          throw new Error("Session creation failed");
+        }
+
+        return sessionToken;
+      }
+    
+      return encode(params);
+    },
+  },
 })
