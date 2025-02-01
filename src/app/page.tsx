9f813bd2d6cbe75191fbf32c2f629d03f2155d24
David Pérez <deividpa022@gmail.com>
2025-02-01 09:04:14 -0500

    feat: implement session check and redirect to sign-in if not authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { SignOut } from "@/components/sign-out";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
+
+  const session = await auth();
+
+  if(!session) redirect("/sign-in");
+
   return (
     <>
       <div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
-        <p className="text-gray-600">Signed in as:</p>
+        <p className="text-gray-600">Signed in as: {session.user?.name}</p>
         <p className="font-medium">TODO</p>
       </div>
 
