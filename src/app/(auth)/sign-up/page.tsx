5b97662e3e5bc5f126e8047ab11386d0b4a20db4
David Pérez <deividpa022@gmail.com>
2025-02-01 13:59:36 -0500

    feat: implement sign-up functionality with session check and redirect

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,16 @@
 import { GithubSignIn } from "@/components/github-sign-in";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { auth } from "@/lib/auth";
+import { signUp } from "@/lib/db/actions";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
+
+  const session = await auth();
+  if (session) redirect("/");
+
   return (
     <div className="w-full max-w-sm mx-auto space-y-6">
       <h1 className="text-2xl font-bold text-center mb-6">Create Account</h1>
@@ -24,8 +31,14 @@ const Page = async () => {
       {/* Email/Password Sign Up */}
       <form
         className="space-y-4"
-        action={async () => {
+        action={async (formData: FormData) => {
           "use server";
+          
+          const res = await signUp(formData);
+
+          if (res.success) {
+            redirect("/");
+          }
         }}
       >
         <Input
